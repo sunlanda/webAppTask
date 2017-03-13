8accfadfc657c9ac4c2794ccb925196784c6b33d
程富碑 <chengfubei@jd.com>
2017-03-13 21:04:25 +0800

    cooking_hot

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
  * Created by chengfubei on 2017/3/12.
  */
 
-const App  = function () {
-    return "666"
-}
+
+import Vue from "vue"
+import App from "./app";
+
+new Vue({
+  el:"#app",
+    render:h=>h(App)
+})
